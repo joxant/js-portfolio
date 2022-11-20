17fb2ab21943d215850ebd792eab206bc24d63ac
joxant <joim117@gmail.com>
2022-11-20 17:14:25 -0300

    configuracion de servidor local

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -13,7 +13,6 @@ module.exports = {
         assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
     mode: 'development',
-    watch: true,
     resolve: {
         extensions: ['.js'],
         alias: {
@@ -78,5 +77,10 @@ module.exports = {
         }),
         new Dotenv(),
     ],
-   
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        compress: true,
+        historyApiFallback: true,
+        port: 3006,
+    },
 }
